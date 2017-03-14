e886793ddeaf3590deb86791ed8c0d45c7f0d47e
Hidowga <hidowga@gmail.com>
2017-03-14 17:02:33 -0300

    teste

--- Person.hs
+++ Person.hs
@@ -6,8 +6,4 @@ data Person= Person { firstName   :: String
                     , height      :: Float
                     , phoneNumber :: String
                     , flavor      :: String
-<<<<<<< HEAD
                          }deriving (Show)
-=======
-                    }deriving (Show)
->>>>>>> e77645f44aa6adee90bb00e87386e155f6e9b47e
