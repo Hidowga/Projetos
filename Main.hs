7d0ca19108f915f4fbe24dc09be3d33bcfa62bc5
Hidowga <hidowga@gmail.com>
2017-03-14 16:09:39 -0300

    gabriela

--- Main.hs
+++ Main.hs
@@ -1,4 +1,4 @@
 module Main where
 import           Person
 main :: IO ()
-main = print$firstName$Person  "Gabriel" "Ariel" 17 1.90 "3330-7320" "morango"
+main = print$firstName$Person  "Gabriela" "Ariel" 17 1.90 "3330-7320" "morango"
